53e056fd3b35d2d141f5465d1ae9cfda37ba9713
Guru4118 <165055820+Guru4118@users.noreply.github.com>
2025-04-03 16:10:15 +0530

    Update Login.jsx

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -16,7 +16,7 @@ export default function LoginPage() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(``${back_url}/api/users/login`, { email, password });
+      const res = await axios.post(`${back_url}/api/users/login`, { email, password });
       localStorage.setItem("user", JSON.stringify(res.data)); // Save user data
       navigate("/"); // Redirect to homepage or cart page
     } catch (error) {
