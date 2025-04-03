ceb8a880f1ba60e95b87ffa2563b6517fefecde1
Guru4118 <165055820+Guru4118@users.noreply.github.com>
2025-04-03 16:12:23 +0530

    Update Signup.jsx

--- frontend/src/Signup.jsx
+++ frontend/src/Signup.jsx
@@ -26,7 +26,7 @@ export default function SignUp() {
       return;
     }
     try {
-      const res = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/users/register`, {
+      const res = await axios.post(`${back_url}/api/users/register`, {
         name: formData.name,
         email: formData.email,
         password: formData.password,
