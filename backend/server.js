8fb37cf3c0dcc4839761a6bc308b81c7fb7f3c88
Guru4118 <165055820+Guru4118@users.noreply.github.com>
2025-04-03 19:35:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,8 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/payment", paymentRoutes);
 
 // Serve React frontend for all non-API routes
-const __dirname = path.resolve();
+const __dirname = __dirname || path.resolve();
+
 app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 app.get("*", (req, res) => {
